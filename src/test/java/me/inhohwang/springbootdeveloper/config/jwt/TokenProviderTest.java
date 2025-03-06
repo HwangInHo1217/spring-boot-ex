c01a6495d82c0ddca8dcdaf1c449db0a54572785
황인호 <hih1217@skuniv.ac.kr>
2025-03-06 17:45:22 +0900

    jwt testing

---
+++ src/test/java/me/inhohwang/springbootdeveloper/config/jwt/TokenProviderTest.java
@@ -0,0 +1,44 @@
+package me.inhohwang.springbootdeveloper.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import me.inhohwang.config.jwt.JwtProperties;
+import me.inhohwang.config.jwt.TokenProvider;
+import me.inhohwang.repository.UserRepository;
+import me.inhohwang.springbootdeveloper.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @DisplayName("generateToken() : 유저 정보와 만료기간을 전달해 토큰을 만들 수 있다.")
+    @Test
+    void generateToken() {
+        //given
+        User testUser = userRepository.save(User.builder()
+                        .email("user@gmail.com")
+                        .password("test")
+                        .build());
+        //when
+        String token = tokenProvider.generateToken(testUser, Duration.ofDays(14));
+        //then
+        Long userId= Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretkey())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id",Long.class);
+        assertThat(userId).isEqualTo(testUser.getId());
+    }
+}
